60d490e11994245ca523fac93c3badfd971e216d
athel4 <athel@isj-technology.com>
2024-02-06 11:34:31 +0800

    saja hui 1

--- APITest/obj/Debug/netstandard2.0/APITest.AssemblyInfo.cs
+++ APITest/obj/Debug/netstandard2.0/APITest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APITest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e22ca62d977dc410a4df7e7a18a161f7f666e16")]
 [assembly: System.Reflection.AssemblyProductAttribute("APITest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APITest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
